c7285d69fe4d7509a012a89e9b7e9eddab25b7d0
Erin Noyes <emnoyes@gmail.com>
2018-04-25 18:19:32 -0700

    added seed data with faker

--- Gemfile
+++ Gemfile
@@ -52,3 +52,5 @@ group :development, :test do
  gem 'dotenv-rails'
 
  gem 'pundit'
+
+ gem 'faker'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,40 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+User.create!(
+  email: 'admin@admin.com',
+  password: 'password',
+  role: :admin
+)
+
+User.create!(
+  email: 'standard@standard.com',
+  password: 'password',
+)
+
+User.create!(
+  email: 'premium@premium.com',
+  password: 'password',
+  role: :premium
+)
+
+10.times do
+  User.create!(
+    email: Faker::Internet.email,
+    password: Faker::Lorem.characters(8)
+  )
+end
+
+users = User.all
+
+20.times do
+  Wiki.create!(
+    title: Faker::Simpsons.character,
+    body: Faker::Simpsons.quote,
+    private: [true, false].sample,
+    user: users.sample
+  )
+end
+
+puts "Seed finished"
+puts "#{Wiki.count} wikis"
+puts "#{User.count} users"
