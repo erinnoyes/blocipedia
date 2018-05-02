3333ce62c384b53f608ceccd9b0a4c86bf14e782
Erin Noyes <emnoyes@gmail.com>
2018-05-02 11:00:01 -0700

    private wikis working

--- app/helpers/wikis_helper.rb
+++ app/helpers/wikis_helper.rb
@@ -1,7 +1,7 @@
 module WikisHelper
 
   def users_wiki_is_private(wiki)
-    current_user && (current_user.admin || current_user.premium?)
+    current_user && (current_user.admin? || current_user.premium?)
   end
 
   def wiki_is_public(wiki)
