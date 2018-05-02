aa156ffd9c6e9d88fce0636656d395fe02ea9a2d
Erin Noyes <emnoyes@gmail.com>
2018-05-01 18:56:45 -0700

    partially done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
 
   include Pundit
     protect_from_forgery with: :exception
-  
+
 
     rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
@@ -12,5 +12,11 @@ class ApplicationController < ActionController::Base
       flash[:alert] = "You are not authorized to do this."
       redirect_to(request.referrer || root_path)
     end
+
+    def revert_users_wikis_to_public
+      current_user.wikis.each do |wiki|
+        wiki.update_attribute(:private, false)
+      end
+    end 
 
 end
--- app/helpers/wikis_helper.rb
+++ app/helpers/wikis_helper.rb
@@ -1,2 +1,9 @@
 module WikisHelper
+  def wiki_is_private_and_user_has_access(wiki)
+    wiki.private? && (current_user == wiki.user || current_user.admin?)
+  end
+
+  def wiki_is_public(wiki)
+    !wiki.private? 
+  end
 end
